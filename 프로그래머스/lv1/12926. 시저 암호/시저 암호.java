16ac13f1eb69e036940ecd24e15b6262af6a6e78
Jang990 <88225377+Jang990@users.noreply.github.com>
2023-01-19 17:10:37 +0900

    [level 1] Title: 시저 암호, Time: 1.00 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,17 +1,12 @@
-import java.util.stream.IntStream;
 class Solution {
     public String solution(String s, int n) {
-        n %= 26;
-        int[] lowerCase = IntStream.range(97, 123).toArray();
-        int[] upperCase = IntStream.range(65, 91).toArray();
-        
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
         	char c = s.charAt(i);
         	if(97 <= c && c <= 122) 
-        		c = (char)lowerCase[(c-97+n)%26];
+        		c = (char)((c-97+n)%26+'a');
         	else if(65 <= c && c <= 90)
-        		c = (char)upperCase[(c-65+n)%26];
+        		c = (char)((c-65+n)%26+'A');
         	sb.append(c);
         }
         
