13f61b249039c2eac25fa21a71a8bf06603777b2
Jang990 <88225377+Jang990@users.noreply.github.com>
2023-08-02 18:53:24 +0900

    [Gold III] Title: 과제, Time: 172 ms, Memory: 15076 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.valueOf(br.readLine());
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        StringTokenizer st;
+        int maxDeadLine = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int deadline = Integer.valueOf(st.nextToken());
+            int score = Integer.valueOf(st.nextToken());
+
+            List<Integer> deadlineList = map.get(deadline);
+            if (deadlineList == null) {
+                deadlineList = new ArrayList<>();
+                deadlineList.add(score);
+                map.put(deadline, deadlineList);
+            }
+            else {
+                deadlineList.add(score);
+            }
+
+            maxDeadLine = Math.max(deadline, maxDeadLine);
+        }
+        br.close();
+
+        List<Integer> now = new ArrayList<>();
+        int result = 0;
+        for (int day = maxDeadLine; day > 0; day--) {
+            List<Integer> deadLineList = map.get(day);
+
+            if (deadLineList != null) {
+                now.addAll(deadLineList);
+                now = now.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+            }
+
+            if (now.isEmpty()) {
+                continue;
+            }
+
+            result += now.get(0);
+            now.remove(0);
+        }
+        System.out.println(result);
+    }
+}
