43defd711925b8dee785da734b38e85fb4eb0ec3
Jang990 <88225377+Jang990@users.noreply.github.com>
2024-06-06 10:16:07 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 31.66 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,53 +1,35 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 class Solution {
-    static final int price = 100;
+    static Map<String, String> graph = new HashMap<>();
+    static Map<String, Integer> index = new HashMap<>();
     static int[] result;
-    private static Map<String, Info> relation;
-    private static final String center = "-";
-
-
     public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         result = new int[enroll.length];
-        relation = new HashMap<>();
         for (int i = 0; i < enroll.length; i++) {
-            relation.put(enroll[i], new Info(i, referral[i]));
+            index.put(enroll[i], i);
+            graph.put(enroll[i], referral[i]);
         }
 
-        sell(seller, amount);
-
+        for (int i = 0; i < seller.length; i++) {
+            sale(seller[i], amount[i] * 100);
+        }
         return result;
     }
 
-    private static void sell(String[] seller, int[] amount) {
-        for (int i = 0; i < seller.length; i++) {
-            Info info = relation.get(seller[i]);
-            int profit = price * amount[i], div;
-            
-            while (true) {
-                div = profit / 10;
-                result[info.idx] += (profit - div);
-
-                if (info.referral.equals(center)) {
-                    break;
-                }
-
-                info = relation.get(info.referral);
-                profit = div;
-                if (div == 0) {
-                    break;
-                }
-            }
+    private static void sale(String seller, int price) {
+        int resultIndex = index.get(seller);
+        if (price <= 1) {
+            result[resultIndex] += price;
+            return;
         }
-    }
 
-    static class Info {
-        int idx;
-        String referral;
+        int div = price / 10;
+        result[resultIndex] += (price - div);
 
-        public Info(int idx, String referral) {
-            this.idx = idx;
-            this.referral = referral;
-        }
+        String parent = graph.get(seller);
+        if(parent.equals("-"))
+            return;
+        sale(parent, div);
     }
 }
