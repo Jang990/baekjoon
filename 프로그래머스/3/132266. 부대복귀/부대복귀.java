5c4c613c2ae13271fedae16675223b27a17b4c5b
Jang990 <88225377+Jang990@users.noreply.github.com>
2024-07-09 10:39:31 +0900

    [level 3] Title: 부대복귀, Time: 61.66 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,54 +2,48 @@ import java.util.*;
 
 class Solution {
     List<Integer>[] graph;
-    int dest;
     int[] min;
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        dest = destination;
-        int[] answer = new int[sources.length];
-        graph = new List[n+1];
         min = new int[n+1];
         Arrays.fill(min, Integer.MAX_VALUE);
-        for(int i = 0; i <= n; i++) {
+        initGraph(n, roads);
+        search(destination);
+
+        int[] result = new int[sources.length];
+        for (int i = 0; i < sources.length; i++) {
+            result[i] = min[sources[i]];
+            if(result[i] == Integer.MAX_VALUE)
+                result[i] = -1;
+        }
+
+        return result;
+    }
+
+    private void initGraph(int n, int[][] roads) {
+        graph = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
             graph[i] = new LinkedList<>();
         }
-        for(int i = 0; i < roads.length; i++) {
-            int n1 = roads[i][0];
-            int n2 = roads[i][1];
-            graph[n1].add(n2);
-            graph[n2].add(n1);
-            /*graph[n1].add(new Edge(n1, n2));
-            graph[n2].add(new Edge(n2, n1));*/
+        
+        for (int[] road : roads) {
+            graph[road[0]].add(road[1]);
+            graph[road[1]].add(road[0]);
         }
-        
-        dijkstra(n, dest);
-        
-        for(int i = 0; i < sources.length; i++) {
-            answer[i] = min[sources[i]];
-            if(answer[i] == Integer.MAX_VALUE) 
-                answer[i] = -1;                
-        }
-        
-        return answer;
     }
-    
-    void dijkstra(int size, int start) {
-        Queue<Integer> qu = new LinkedList();
-        qu.offer(start);
-        
-        boolean[] visited = new boolean[size+1];
-        min[start] = 0;
-        
-        while(!qu.isEmpty()) {
+
+    private void search(int destination) {
+        Queue<Integer> qu = new LinkedList<>();
+        min[destination] = 0;
+        qu.offer(destination);
+
+        while (!qu.isEmpty()) {
             int now = qu.poll();
-            if(visited[now]) continue;
-            visited[now] = true;
-            for(int next : graph[now]) {
-                if(min[now] + 1 >= min[next])
+            for (int next : graph[now]) {
+                if(min[next] != Integer.MAX_VALUE && min[next] <= min[now] + 1)
                     continue;
                 min[next] = min[now] + 1;
                 qu.offer(next);
             }
         }
-    } 
+    }
 }
