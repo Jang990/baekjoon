2e533d690e8a362b6179cb0f004729b3c7bed234
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-06-10 17:12:09 +0900

    [Gold III] Title: 줄 세우기, Time: 676 ms, Memory: 76420 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,58 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class Main {
+    static int[] in;
     static List<Integer>[] graph;
-    static int[] enter;
     static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        int n = Integer.parseInt(line[0]);
-        int m = Integer.parseInt(line[1]);
-        graph = new List[n+1];
-        enter = new int[n+1];
-        visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++)
-            graph[i] = new ArrayList<>();
+        int[] input = readLine(br);
+        int N = input[0];
+        int M = input[1];
+        graph = new List[N + 1];
+        in = new int[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 0; i <= N; i++)
+            graph[i] = new LinkedList<>();
 
-        for (int i = 0; i < m; i++) {
-            line = br.readLine().split(" ");
-            int n1 = Integer.parseInt(line[0]);
-            int n2 = Integer.parseInt(line[1]);
-            graph[n1].add(n2);
-            enter[n2]++;
+        for (int i = 0; i < M; i++) {
+            input = readLine(br);
+            graph[input[0]].add(input[1]);
+            in[input[1]]++;
         }
+
         br.close();
 
-        StringBuilder sb = new StringBuilder();
-        sort(sb);
-        System.out.println(sb);
+        System.out.println(search());
     }
 
-    private static void sort(StringBuilder sb) {
-        Queue<Integer> qu = new LinkedList<>();
-        for (int i = 1; i < enter.length; i++) {
-            for (int j = 1; j < enter.length; j++) {
-                if (!visited[j] && enter[j] == 0) {
-                    qu.offer(j);
-                    visited[j] = true;
-                }
-            }
+    private static String search() {
+        StringBuilder sb = new StringBuilder();
 
-            while (!qu.isEmpty()) {
-                int now = qu.poll();
-                sb.append(now).append(" ");
-                List<Integer> list = graph[now];
-                for (int next : list) {
-                    enter[next]--;
-                }
+        Queue<Integer> qu = findZero();
+        while (!qu.isEmpty()) {
+            int zeroNode = qu.poll();
+            for (Integer nextNode : graph[zeroNode]) {
+                in[nextNode]--;
+                if(in[nextNode] == 0)
+                    qu.add(nextNode);
             }
+            sb.append(zeroNode).append(" ");
         }
+
+        return sb.toString();
+    }
+
+    private static LinkedList<Integer> findZero() {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 1; i < in.length; i++) {
+            if(in[i] == 0 && !visited[i])
+                list.add(i);
+        }
+        return list;
+    }
+
+    private static int[] readLine(BufferedReader br) throws IOException {
+        return Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
