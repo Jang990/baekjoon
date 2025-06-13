2c71c90500b99781c22c761e764624bcad79e745
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-06-13 12:04:49 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 20272 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,55 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-
-    private static final int MAX_SIZE = 100_000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int soobin = Integer.parseInt(st.nextToken());
-        int goal = Integer.parseInt(st.nextToken());
+        String[] line = br.readLine().split(" ");
         br.close();
 
-        System.out.println(bfs(soobin, goal));
+        System.out.println(bfs(Integer.parseInt(line[0]), Integer.parseInt(line[1])));
     }
 
-    private static int bfs(int soobin, int goal) {
+    private static int bfs(int start, int end) {
+        int[] visited = new int[100_001];
+        Arrays.fill(visited, Integer.MAX_VALUE);
         Queue<Integer> qu = new LinkedList<>();
-        int[] visited = new int[MAX_SIZE + 1];
-        int[] dir = {1, -1};
-        visited[soobin] = 1;
-        qu.offer(soobin);
 
+        visited[start] = 0;
+        qu.offer(start);
         while (!qu.isEmpty()) {
-            int now = qu.poll();
-            if(now == goal)
-                break;
+            int current = qu.poll();
 
-            if (!outOfBound(now * 2)
-                    && (visited[now*2] == 0 || visited[now * 2] > visited[now])) {
-                visited[now * 2] = visited[now];
-                qu.offer(now * 2);
-            }
-
-            for (int i = 0; i < 2; i++) {
-                int next = now + dir[i];
-                if(outOfBound(next) || (visited[next] != 0 && visited[next] <= visited[now] + 1))
-                    continue;
-                visited[next] = visited[now] + 1;
+            int next = current * 2;
+            if (isInBound(next) && visited[next] > visited[current]) {
+                visited[next] = visited[current];
                 qu.offer(next);
             }
+
+            move(visited, qu, current, current - 1);
+            move(visited, qu, current, current + 1);
         }
 
-         return visited[goal] - 1;
+        return visited[end];
     }
 
-    private static boolean outOfBound(int next) {
-        return 0 > next || next > MAX_SIZE;
+    private static void move(int[] visited, Queue<Integer> qu, int current, int next) {
+        if (isInBound(next) && visited[next] > visited[current]) {
+            visited[next] = visited[current] + 1;
+            qu.offer(next);
+        }
+    }
+
+    private static boolean isInBound(int loc) {
+        return 0 <= loc && loc <= 100_000;
     }
 }
