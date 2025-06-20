2b503f5d807cf71238bece84abd53d15c4c0ee88
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-06-20 11:33:47 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29044 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,30 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.valueOf(st.nextToken());
-        int C = Integer.valueOf(st.nextToken());
+        String[] line = br.readLine().split(" ");
+        int N = Integer.parseInt(line[0]);
+        int C = Integer.parseInt(line[1]);
         int[] arr = new int[N];
-        arr[0] = Integer.valueOf(br.readLine());
-        for (int i = 1; i < N; i++) {
-            arr[i] = Integer.valueOf(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
         br.close();
 
         Arrays.sort(arr);
-        int left = 1, right = arr[N-1] - arr[0] + 1;
-        for (int i = 1; i < N; i++) {
-            left = Math.min(left, arr[i] - arr[i - 1]);
-        }
-
+        int left = 1;
+        int right = arr[arr.length - 1] - arr[0] + 1;
+        int mid = -1;
         while (left < right) {
-            int mid = (left + right) / 2;
-            int cnt = getInstalledHouseCount(arr, mid);
+            mid = (left + right) / 2;
+            int cnt = calculate(mid, arr);
             if(cnt < C)
                 right = mid;
             else
@@ -35,16 +31,17 @@ public class Main {
         System.out.println(left - 1);
     }
 
-    private static int getInstalledHouseCount(int[] arr, int shortestDistance) {
-        int cnt = 1;
-        int lastInstalledLocation = arr[0];
+    private static int calculate(int minLen, int[] arr) {
+        int result = 1;
+        int before = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            int distance = arr[i] - lastInstalledLocation;
-            if (distance >= shortestDistance) {
-                cnt++;
-                lastInstalledLocation = arr[i];
-            }
+            int len = arr[i] - before;
+            if(minLen > len)
+                continue;
+            before = arr[i];
+            result++;
         }
-        return cnt;
+
+        return result;
     }
 }
