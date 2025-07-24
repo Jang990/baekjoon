992427e4142871f7574764c773961c35117594ac
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-07-24 11:50:01 +0900

    [Gold IV] Title: 여행 가자, Time: 180 ms, Memory: 18304 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -7,58 +7,54 @@ public class Main {
     static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int planCnt = Integer.parseInt(br.readLine());
-        initParent(n);
-        for (int i = 0; i < n; i++) {
-            String[] arr = br.readLine().split(" ");
-            for (int j = 0; j < n; j++) {
-                if(arr[j].equals("1")) {
-                    union(i + 1, j + 1);
-                }
+        int N = Integer.parseInt(br.readLine());
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int[] line = readLine(br);
+            for (int j = 0; j < line.length; j++) {
+                if(line[j] == 0)
+                    continue;
+                union(i, j + 1);
             }
         }
-        int[] plan = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).toArray();
+        int[] plan = readLine(br);
         br.close();
 
-        if(isOk(plan))
-            System.out.println("YES");
-        else
-            System.out.println("NO");
-    }
-
-    private static boolean isOk(int[] plan) {
-        int startParent = parent[plan[0]];
+        int root = findParent(plan[0]);
         for (int i = 1; i < plan.length; i++) {
-            if(startParent != find(plan[i]))
-                return false;
+            if (findParent(plan[i]) != root) {
+                System.out.println("NO");
+                return;
+            }
         }
 
-        return true;
+        System.out.println("YES");
+    }
+
+    private static int findParent(int n) {
+        if (parent[n] == n)
+            return n;
+        return findParent(parent[n]);
     }
 
     private static void union(int n1, int n2) {
-        int n1Parent = find(n1);
-        int n2Parent = find(n2);
+        int n1Parent = findParent(n1);
+        int n2Parent = findParent(n2);
 
-        if(n1Parent == n2Parent) return;
-        if(n1Parent > n2Parent)
+        if (n1Parent > n2Parent) {
             parent[n1Parent] = n2Parent;
-        else
+        } else {
             parent[n2Parent] = n1Parent;
-    }
-
-    private static int find(int n) {
-        if(parent[n] == n)
-            return n;
-        return parent[n] = find(parent[n]);
-    }
-
-    private static void initParent(int n) {
-        parent = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
         }
+    }
+
+
+    private static int[] readLine(BufferedReader br) throws IOException {
+        return Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 }
