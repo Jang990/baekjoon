c2af65cce68f6ddb8949be3023b528650b9434c2
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-11-04 11:22:22 +0900

    [Silver I] Title: 트럭, Time: 116 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -4,55 +4,51 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int truckCnt = Integer.parseInt(st.nextToken());
-        int bridgeLength = Integer.parseInt(st.nextToken());
-        int maxLoad = Integer.parseInt(st.nextToken());
+        int[] input = readLine(br);
+        int truckCnt = input[0];
+        int bridgeLen = input[1];
+        int bridgeMaxWeight = input[2];
 
-        int[] truck = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::valueOf).toArray();
+        int[] truckWeights = readLine(br);
         br.close();
 
-        Queue<Truck> bridge = new LinkedList<>();
-        int time = 1;
-        int load = 0;
-        int nowTruckIdx = 0;
-        while (nowTruckIdx < truckCnt) {
-            if (bridge.peek() != null && time - bridge.peek().startTime == bridgeLength) {
-                load -= bridge.poll().weight;
+        Queue<TruckTimeLine> qu = new LinkedList<>();
+        int time = 0;
+        int nextTruckIdx = 0;
+        int bridgeCurrentWeight = 0;
+        while (nextTruckIdx != truckCnt) {
+            time++;
+            if (!qu.isEmpty() && time - qu.peek().enterTime == bridgeLen) {
+                TruckTimeLine exitTruck = qu.poll();
+                bridgeCurrentWeight -= exitTruck.weight;
             }
 
-            if(load + truck[nowTruckIdx] <= maxLoad) {
-                load += truck[nowTruckIdx];
-                bridge.add(new Truck(time, truck[nowTruckIdx]));
-                nowTruckIdx++;
-            }
-
-            time++;
-        }
-
-        while (!bridge.isEmpty()) {
-            time++;
-            if (time - bridge.peek().startTime == bridgeLength) {
-                bridge.poll();
+            if (bridgeCurrentWeight + truckWeights[nextTruckIdx] <= bridgeMaxWeight) {
+                qu.offer(new TruckTimeLine(truckWeights[nextTruckIdx], time));
+                bridgeCurrentWeight += truckWeights[nextTruckIdx];
+                nextTruckIdx++;
             }
         }
-
-        System.out.println(time);
+        System.out.println(time + bridgeLen);
     }
 
-    static class Truck {
-        int startTime;
-        int weight;
+    private static int[] readLine(BufferedReader br) throws IOException {
+        return Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
 
-        public Truck(int startTime, int weight) {
-            this.startTime = startTime;
+    static class TruckTimeLine {
+        int weight;
+        int enterTime;
+
+        public TruckTimeLine(int weight, int enterTime) {
             this.weight = weight;
+            this.enterTime = enterTime;
         }
     }
 }
