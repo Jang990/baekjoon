411dbfb501a8f5faff1a9dd100e4f3cbe49047d4
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-11-05 14:07:32 +0900

    [Gold V] Title: 배, Time: 3044 ms, Memory: 17004 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,44 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] crane;
-		List<Integer> cargo;
-		
-		br.readLine();
-		crane = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).sorted().toArray();
-		br.readLine();
-		cargo = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).sorted().boxed().collect(Collectors.toList());
-		br.close();
-		
-		if(cargo.get(cargo.size()-1) > crane[crane.length-1]) {
-			System.out.println(-1);
-			return;
-		}
-		
-		int count = 0;
-		while(!cargo.isEmpty()) {
-			
-			for (int i = 0; i < crane.length; i++) {
-				for (int j = cargo.size()-1; 0 <= j; j--) {
-					if(crane[i] < cargo.get(j))
-						continue;
-					
-					cargo.remove(j);
-					break;
-				}
-			}
-			count++;
-		}
-		
-		System.out.println(count);
-	}
+    private static int[] crane;
+    private static List<Integer> box;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 50 - 10_000
+        // 무거운거 먼저 처리한다. + 가벼운 크레인이 먼저 가져간다.
+        // 가벼운 크레인이 무거운 박스를 먼저 집는다.
+
+        int N = Integer.parseInt(br.readLine());
+        crane = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+        int M = Integer.parseInt(br.readLine());
+        box = readBox(br);
+
+        br.close();
+
+        if (crane[crane.length - 1] < box.get(0)) {
+            System.out.println(-1);
+            return;
+        }
+
+        int time = 0;
+        while (!box.isEmpty()) {
+            time++;
+            for (int craneIdx = 0; craneIdx < N; craneIdx++) {
+                for (int boxIdx = 0; boxIdx < box.size(); boxIdx++) {
+                    if(crane[craneIdx] < box.get(boxIdx))
+                        continue;
+                    box.remove(boxIdx);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(time);
+    }
+
+    private static List<Integer> readBox(BufferedReader br) throws IOException {
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
 }
