39bfaabad5f6471eb56499d6f7975ea94e181c79
Jang990 <88225377+Jang990@users.noreply.github.com>
2025-11-06 16:02:14 +0900

    [Silver II] Title: 비슷한 단어, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,79 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         String base = br.readLine();
-        Map<Character, Integer> baseMap = new HashMap<>();
-        for (int i = 0; i < base.length(); i++) {
-            char key = base.charAt(i);
-            baseMap.put(key, baseMap.getOrDefault(key,0) + 1);
-        }
+        int[] baseArr = toArr(base);
 
-        // 하나의 문자를 빼거나, 바꾸거나 = 비슷함
-        int result = 0;
-        for (int i = 0; i < n-1; i++) {
-            String line = br.readLine();
-            if (base.length() - 1 > line.length() || line.length() > base.length() + 1) {
-                continue;
-            }
-
-            Map<Character, Integer> targetMap = new HashMap<>();
-            for (int j = 0; j < line.length(); j++) {
-                char c = line.charAt(j);
-                targetMap.put(c, targetMap.getOrDefault(c,0) + 1);
-            }
-
-            if(isSimilarWord(baseMap, targetMap)) {
-                result++;
-            }
+        int similarCnt = 0;
+        for (int i = 1; i < n; i++) {
+            String word = br.readLine();
+            int[] wordArr = toArr(word);
+            if(isSimilar(baseArr, wordArr))
+                similarCnt++;
         }
         br.close();
 
-        System.out.println(result);
+        System.out.println(similarCnt);
     }
 
-    private static boolean isSimilarWord(Map<Character, Integer> baseMap, Map<Character, Integer> targetMap) {
-        int wrongSpe = 0;
-        int difference = 0;
-
-        for (Character c : targetMap.keySet()) {
-            int baseCnt = baseMap.getOrDefault(c, 0);
-            int targetCnt = targetMap.getOrDefault(c, 0);
-
-            if (baseCnt == targetCnt) {
+    private static boolean isSimilar(int[] baseArr, int[] wordArr) {
+        int moreCnt = 0;
+        int lessCnt = 0;
+        for (int i = 0; i < 26; i++) {
+            if(baseArr[i] == wordArr[i])
                 continue;
-            }
-
-            if (baseCnt == 0) {
-                wrongSpe += targetCnt;
-            }
-            else {
-                difference += Math.abs(baseCnt - targetCnt);
-            }
+            if(baseArr[i] > wordArr[i])
+                lessCnt += (baseArr[i] - wordArr[i]);
+            else
+                moreCnt += (wordArr[i] - baseArr[i]);
         }
 
-        HashSet<Character> cloneBaseKeySet = new HashSet<>(baseMap.keySet());
-        cloneBaseKeySet.removeAll(targetMap.keySet());
+        boolean isSame = moreCnt == 0 && lessCnt == 0;
+        boolean isCanChanged = moreCnt == 1 && lessCnt == 1;
+        boolean isCanRemoved = moreCnt == 1 && lessCnt == 0;
+        boolean isCanAdded = moreCnt == 0 && lessCnt == 1;
+        return isSame || isCanChanged || isCanRemoved || isCanAdded;
+    }
 
-        for (Character c : cloneBaseKeySet) {
-            difference += baseMap.get(c);
+    private static int[] toArr(String base) {
+        int[] baseArr = new int[26];
+        for (int i = 0; i < base.length(); i++) {
+            int idx = base.charAt(i) - 'A';
+            baseArr[idx]++;
         }
-
-        if (wrongSpe > 1) {
-            return false;
-        }
-
-        if (difference > 1 && (difference != 2 || wrongSpe != 0)) {
-            return false;
-        }
-
-        return true;
+        return baseArr;
     }
 }
