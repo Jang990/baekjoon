aeb32c86b58903de7a1fa43bc93fb4e3d622feb0
Jang990 <88225377+Jang990@users.noreply.github.com>
2026-02-01 14:11:14 +0900

    [Silver III] Title: 크로스 컨트리, Time: 160 ms, Memory: 16936 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,81 +6,84 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        while (testCase-- > 0) {
-            HashMap<Integer, Team> teams = new HashMap<>();
-            int timeLimit = Integer.parseInt(br.readLine());
-            int[] timeline = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
+        Team winner = null;
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            ArrayList<Integer> teamIds = findTeamId(arr);
+            HashMap<Integer, Team> teamIdAndTeam = new HashMap<>();
+            for (int teamId : teamIds)
+                teamIdAndTeam.put(teamId, new Team(teamId));
 
-            for (int time = 0; time < timeLimit; time++) {
-                int teamId = timeline[time];
-                if(!teams.keySet().contains(teamId))
-                    teams.put(teamId, new Team(teamId));
-                teams.get(teamId).enter(time);
+            int score = 1;
+            for (int teamId : arr) {
+                if (!teamIdAndTeam.containsKey(teamId))
+                    continue;
+                teamIdAndTeam.get(teamId).add(score++);
             }
 
-            int currentScore = 1;
-            for (int time = 0; time < timeLimit; time++) {
-                int teamId = timeline[time];
-                if (teams.get(teamId).addScore(currentScore))
-                    currentScore++;
-            }
+            winner = null;
+            int minSumTop1To4 = Integer.MAX_VALUE;
+            int minTop5 = Integer.MAX_VALUE;
+            for (Team team : teamIdAndTeam.values()) {
+                if (team.sumTop1To4() > minSumTop1To4)
+                    continue;
+                if (team.sumTop1To4() == minSumTop1To4 && team.getTop5() > minTop5)
+                    continue;
 
-            int winningTeamId =  teams.values().stream()
-                    .filter(t -> t.getScore() != -1)
-                    .sorted((t1, t2) -> {
-                        if (t1.getScore() != t2.getScore())
-                            return Integer.compare(t1.getScore(), t2.getScore());
-                        return Integer.compare(t1.enterTime.get(4), t2.enterTime.get(4));
-                    }).findFirst().get().id;
-            sb.append(winningTeamId).append("\n");
+                winner = team;
+                minSumTop1To4 = team.sumTop1To4();
+                minTop5 = team.getTop5();
+            }
+            sb.append(winner.id).append("\n");
         }
         br.close();
 
         System.out.println(sb);
     }
 
+    /*
+    상위 4명의 주자 점수 합산 계산
+    6명이 참가하지 못한 경우 점수 계산에서 제외
+    가장 낮은 점수 팀 우승
+    동점일 경우 5등이 빨리 들어온 팀 우승
+     */
+
+    private static ArrayList<Integer> findTeamId(int[] arr) {
+        HashMap<Integer, Integer> teamIdAndCnt = new HashMap<>();
+        for (int teamId : arr) {
+            teamIdAndCnt.put(teamId, teamIdAndCnt.getOrDefault(teamId, 0) + 1);
+        }
+
+        ArrayList<Integer> teamIds = new ArrayList<>();
+        for (int teamId : teamIdAndCnt.keySet()) {
+            if(teamIdAndCnt.get(teamId) >= 6)
+                teamIds.add(teamId);
+        }
+        return teamIds;
+    }
+
+
     static class Team {
-        int id;
-        ArrayList<Integer> enterTime = new ArrayList<>();
-        int scoreCnt = 0;
-        int score = 0;
+        private int id;
+        private ArrayList<Integer> scores = new ArrayList<>();
 
         public Team(int id) {
             this.id = id;
         }
 
-        public void enter(int time) {
-            enterTime.add(time);
+        public void add(int score) {
+            scores.add(score);
         }
 
-        public int getScore() {
-            if(enterTime.size() < 6)
-                return -1;
-            return score;
+        public int sumTop1To4() {
+            return scores.get(0) + scores.get(1) + scores.get(2) + scores.get(3);
         }
 
-        public boolean addScore(int score) {
-            if(enterTime.size() < 6)
-                return false;
-            if (scoreCnt < 4) {
-                this.score += score;
-                scoreCnt++;
-            }
-            return true;
-        }
-
-        @Override
-        public String toString() {
-            return "Team{" +
-                    "id=" + id +
-                    ", enterTime=" + enterTime +
-                    ", score=" + getScore() +
-                    '}';
+        public int getTop5() {
+            return scores.get(4);
         }
     }
 }
