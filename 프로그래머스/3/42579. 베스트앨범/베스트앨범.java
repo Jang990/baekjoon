a47f5b2ec2f3df7986e8479453c5623669d676ae
Jang990 <88225377+Jang990@users.noreply.github.com>
2026-06-02 18:04:30 +0900

    [level 3] Title: 베스트앨범, Time: 9.15 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,70 +1,44 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public static int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> map = new HashMap<>();
-        Map<String, List<Music>> listMap = new HashMap<>();
-
+        HashMap<String, Integer> sum = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
-            map.put(genres[i],map.getOrDefault(genres[i], 0) + plays[i]);
-            List<Music> list = listMap.getOrDefault(genres[i], new LinkedList<>());
-            list.add(new Music(i, plays[i]));
-            listMap.put(genres[i], list);
+            sum.put(genres[i], sum.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-        List<Integer> result = new LinkedList<>();
-        int size = map.size();
-        for (int i = 0; i < size; i++) {
-            String key = getMaxGenre(map);
-            map.remove(key);
-            List<Music> musics = listMap.get(key);
-            musics.sort(getMusicComparator());
-            result.add(musics.get(0).idx);
-            if(musics.size() >= 2)
-                result.add(musics.get(1).idx);
-        }
+        List<String> rank = sum.keySet().stream()
+                .sorted(Comparator.comparingInt(sum::get).reversed())
+                .collect(Collectors.toList());
 
-        return result.stream().mapToInt(Integer::valueOf).toArray();
-    }
+        List<Integer> result = new ArrayList<>();
+        for (String genre : rank) {
+            int top1Idx = -1, top2Idx = -1;
+            int top1Play = -1, top2Play = -1;
+            for (int i = 0; i < genres.length; i++) {
+                if(!genres[i].equals(genre))
+                    continue;
+                if (plays[i] > top1Play) {
+                    top2Play = top1Play;
+                    top2Idx = top1Idx;
 
-    private static Comparator<Music> getMusicComparator() {
-        return (m1, m2) -> {
-            if (m1.play == m2.play) {
-                if (m1.idx > m2.idx) {
-                    return 1;
+                    top1Play = plays[i];
+                    top1Idx = i;
+                    continue;
                 }
-                return -1;
+
+                if (plays[i] > top2Play) {
+                    top2Play = plays[i];
+                    top2Idx = i;
+                }
             }
-
-            if (m1.play > m2.play) {
-                return -1;
-            } else {
-                return 1;
-            }
-        };
-    }
-
-    private static String getMaxGenre(Map<String, Integer> map) {
-        String result = "";
-        int max = Integer.MIN_VALUE;
-        for (String s : map.keySet()) {
-            int val = map.get(s);
-            if (val <= max)
-                continue;
-
-            result = s;
-            max = val;
+            result.add(top1Idx);
+            if(top2Idx != -1)
+                result.add(top2Idx);
         }
 
-        return result;
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    static class Music {
-        int idx;
-        int play;
-
-        public Music(int idx, int play) {
-            this.idx = idx;
-            this.play = play;
-        }
-    }
 }
